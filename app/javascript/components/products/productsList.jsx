4c183c8ec4377f8f9ef72cfe78ed7ad71830a0c7
sai43 <chsai.btech@gmail.com>
2019-05-02 21:28:32 +0530

    fetching data from rails API end

--- app/javascript/components/products/productsList.jsx
+++ app/javascript/components/products/productsList.jsx
@@ -1,6 +1,37 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types';
+import ProductCard from './productCard'
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+// import GridListTile from '@material-ui/core/GridListTile';
+// import GridListTileBar from '@material-ui/core/GridListTileBar';
+// import IconButton from '@material-ui/core/IconButton';
+// import StarBorderIcon from '@material-ui/icons/StarBorder';
+// import tileData from './tileData';
 
-import Card from './card'
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+        width: 960,
+        height: 600,
+    },
+    title: {
+        color: theme.palette.primary.light,
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+});
 
 class ProductsList extends  Component {
 
@@ -13,37 +44,27 @@ class ProductsList extends  Component {
     }
 
     getAllProducts = () => {
-        const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
-        fetch('/graphql', {
-            method: 'POST',
-            headers: {'content-type': 'application/json', 'X-CSRF-Token': csrfToken},
-            body: JSON.stringify(
-                { query: `
-                    query {
-                          products {
-                            id
-                            title
-                            price
-                          }  
-                        }
-                ` })
-        }).then(response => {
-            return response.json()
-        }).then(response => {
-            this.setState({products: response.data.products})
-        })
+        $.getJSON('/api/v1/products/index', (response) => { this.setState({ products: response }) });
     };
 
     render () {
+        const { classes } = this.props;
         const products = this.state.products;
         return (
-          <div>
-              <Card />
-              {products.map((product, index )=>( <h2>{ product.title } - { product.price }</h2> ))}
-          </div>
+            <div className={classes.root}>
+                <GridList cellHeight={160} className={classes.gridList} cols={3}>
+                    { products.map((item, index )=>(
+                        <ProductCard key={index} product={item} />
+                    ))}
+                </GridList>
+            </div>
         );
     }
 
 }
 
-export default ProductsList;
+ProductsList.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductsList);
